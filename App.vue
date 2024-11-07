6817d6f5e06a0f24d53166c4468847289c1c252c
한재경 <hjg000223@gmail.com>
2024-11-07 13:57:22 +0900

    feat: 지도 뷰에서 헤더 제거

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <!-- Welcome 페이지가 아닐 때만 Header와 BottomNav를 표시합니다 -->
+    <!-- 특정 페이지가 아닐 때만 Header와 BottomNav를 표시합니다 -->
     <Header v-if="showHeaderAndNav" />
     <router-view />
     <BottomNav v-if="showHeaderAndNav" />
@@ -13,9 +13,9 @@ import { computed } from 'vue';
 import Header from './src/components/Header.vue';
 import BottomNav from './src/components/BottomNav.vue';
 
-// 현재 라우터 경로를 가져와 Welcome 페이지인지 확인
+// 현재 라우터 경로를 가져와 특정 페이지인지 확인
 const route = useRoute();
-const showHeaderAndNav = computed(() => route.name !== 'Welcome');
+const showHeaderAndNav = computed(() => route.name !== 'RecommendedFacilities'); // 'RecommendedFacilities' 페이지에서는 헤더와 네비게이션 바를 숨깁니다
 </script>
 
 <style scoped>
