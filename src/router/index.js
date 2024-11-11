1c5bc04c58324a39ce766b4bf65368607e3fa4c0
한재경 <hjg000223@gmail.com>
2024-11-11 15:13:52 +0900

    feat: 시설추천용 라우터 추가

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,16 @@ import DisabilityTypeSelection from '@/components/signin/DisabilityTypeSelection
 import DisabilityGradeSelection from '@/components/signin/DisabilityGradeSelection.vue';
 import RegistrationInput from '@/components/signin/RegistrationInput.vue';
 import IndoorExercise from '@/components/IndoorExercise.vue';
-import OutdoorExercise from '@/components/OutdoorExercise.vue';
 import RecommendedFacilities from '@/components/RecommendedFacilities.vue';
-import Login from '@/components/login.vue';
+import Login from '@/components/Login.vue';
+import DistanceSelection from '@/components/facilitytraining/DistanceSelection.vue';
+import Question from '@/components/facilitytraining/Question.vue';
+import RecommendedExercise from '@/components/facilitytraining/RecommendedExercise.vue';
 
 const routes = [
   { path: '/', name: 'Welcome', component: Welcome },
   { path: '/home', name: 'Home', component: Home },
-  { path: '/login', name: 'Login', component: Login},
+  { path: '/login', name: 'Login', component: Login },
   { path: '/age-selection', name: 'AgeSelection', component: AgeSelection },
   { path: '/gender-selection', name: 'GenderSelection', component: GenderSelection },
   { path: '/disability-type-selection', name: 'DisabilityTypeSelection', component: DisabilityTypeSelection },
@@ -30,14 +32,25 @@ const routes = [
     component: IndoorExercise,
   },
   {
-    path: '/outdoor-exercise',
-    name: 'OutdoorExercise',
-    component: OutdoorExercise,
-  },
-  {
     path: '/recommended-facilities',
     name: 'RecommendedFacilities',
     component: RecommendedFacilities,
+  },
+  {
+    path: '/distance-selection',
+    name: 'DistanceSelection',
+    component: DistanceSelection,
+  },
+  {
+    path: '/question',
+    name: 'Question',
+    component: Question,
+    props: true,
+  },
+  {
+    path: '/recommended-exercise',
+    name: 'RecommendedExercise',
+    component: RecommendedExercise,
   },
 ];
 
