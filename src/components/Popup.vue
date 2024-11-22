6803e7e99f5ea69f4dc3dc57d7d281c8115dc9db
조준희 <junh9541@gmail.com>
2024-11-22 11:20:15 +0900

    feat: 가정운동 ui 변경, 팝업 취소버튼 추가

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="popup-overlay" v-if="isVisible">
     <div class="popup">
-      <p>운동을 추가했습니다.<br />홈화면에서 운동 후 시간을 기록해 주세요!</p>
-      <button class="confirm-button" @click="addCalendar">확인</button>
+      <p>운동을 추가할까요?</p>
+      <div class="btn-conatainer">
+        <button class="confirm-button" @click="addCalendar">확인</button>
+        <button class="cancle-button" @click="emit('close')">취소</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { toRefs } from "vue";
-import apiClient from "@/axios/apiClient.js";
+import { toRefs } from 'vue';
+import apiClient from '@/axios/apiClient.js';
 
 // Props 및 Emits 정의
 const props = defineProps({
@@ -29,24 +32,27 @@ const props = defineProps({
 
 const { exercise, exerciseType } = toRefs(props);
 
-const emit = defineEmits(["close"]);
+
+const emit = defineEmits(['close']);
 
 // 캘린더에 운동 추가 함수
 async function addCalendar() {
   try {
+    console.log(exercise.value)
+    console.log(exerciseType.value)
     const requestData = {
       exerciseId: exercise.value.exerciseId,
       duration: 0,
-      exerciseType: exerciseType.value, // this.exerciseType 대신 간단하게 사용
+      exerciseType, // this.exerciseType 대신 간단하게 사용
     };
-    console.log(requestData);
-    await apiClient.post("calendar", requestData);
-    emit("close"); // 모달 닫기 이벤트 발생
+    await apiClient.post('calendar', requestData);
+    emit('close'); // 모달 닫기 이벤트 발생
   } catch (error) {
-    console.error("Failed to add exercise:", error);
+    console.error('Failed to add exercise:', error);
   }
 }
 </script>
+
 
 <style scoped>
 .popup-overlay {
@@ -81,5 +87,22 @@ async function addCalendar() {
   cursor: pointer;
   border-radius: 8px;
   font-size: 16px;
+}
+.cancle-button{
+  margin-top: 16px;
+  background-color: #b3b3b3;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  border-radius: 8px;
+  font-size: 16px;
+}
+
+.btn-conatainer{
+  display: flex;
+  gap: 16px; /* 버튼 사이 간격 */
+  justify-content: center; /* 버튼을 가운데 정렬 */
+  margin-top: 16px; /* 버튼 컨테이너와 위 요소 간격 */
 }
 </style>
