2ff84fb0bfb3486f266ad9490b0dc5470601a467
한재경 <hjg000223@gmail.com>
2024-11-22 10:55:00 +0900

    feat: welcome뷰 시작화면 디자인 변경

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="welcome-container">
+    <img class="logo" src="@/assets/fitable.png" alt="Fitable Logo" />
     <h1 class="title">Fitable</h1>
     <p class="description">
       실내외 운동으로 건강을 챙기세요!<br />
@@ -9,7 +10,7 @@
     <button class="button" @click="navigateToHome">로그인</button>
   </div>
 </template>
-  
+
 <script setup>
 import { useRouter } from 'vue-router';
 
@@ -17,7 +18,7 @@ const router = useRouter();
 
 // 회원가입 페이지로 이동
 function navigateToRegister() {
-  router.push({ name: 'AgeSelection' }); // 변경된 라우트 이름으로 이동
+  router.push({ name: 'AgeSelection' });
 }
 
 // 로그인 페이지로 이동
@@ -26,46 +27,53 @@ function navigateToHome() {
 }
 </script>
 
-  <style scoped>
-  .welcome-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    padding: 20px;
-    height: 100vh;
-  }
-  
-  .title {
-    font-size: 36px;
-    font-weight: bold;
-    color: #333333;
-    margin-bottom: 16px;
-  }
-  
-  .description {
-    font-size: 16px;
-    color: #666666;
-    margin-bottom: 40px;
-  }
-  
-  .button {
-    width: 90%;
-    height: 60px;
-    padding: 12px;
-    font-size: 18px;
-    color: #ffffff;
-    background-color: #4CAF50;
-    border: none;
-    border-radius: 8px;
-    cursor: pointer;
-    margin: 8px 0;
-    text-align: center;
-  }
-  
-  .button:hover {
-    background-color: #45a049;
-  }
-  </style>
-  
+<style scoped>
+.welcome-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 20px;
+  height: 100vh;
+  background-color: #ffffff;
+}
+
+.logo {
+  width: 380px; /* 로고 크기 조정 */
+  height: auto;
+  padding-top: 30px;
+  margin-bottom: 24px;
+}
+
+.title {
+  font-size: 36px;
+  font-weight: bold;
+  color: #333333;
+  margin-bottom: 16px;
+}
+
+.description {
+  font-size: 16px;
+  color: #666666;
+  margin-bottom: 40px;
+}
+
+.button {
+  width: 90%;
+  height: 60px;
+  padding: 12px;
+  font-size: 18px;
+  color: #ffffff;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  margin: 8px 0;
+  text-align: center;
+}
+
+.button:hover {
+  background-color: #45a049;
+}
+</style>
